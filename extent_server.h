9f022f7e68b6bea28908d2f2877df9a78071fb8b
Moritz Wilhelm <s8mowilh@stud.uni-saarland.de>
2021-05-27 21:20:09 +0200

    reformatted lab2 files
    Co-authored-by: Daniel Emmel <s8daemme@stud.uni-saarland.de>

--- extent_server.h
+++ extent_server.h
@@ -9,20 +9,16 @@
 
 class extent_server {
 
- public:
-  extent_server();
+public:
+    extent_server();
 
-  int put(extent_protocol::extentid_t id, std::string, int &);
-  int get(extent_protocol::extentid_t id, std::string &);
-  int getattr(extent_protocol::extentid_t id, extent_protocol::attr &);
-  int remove(extent_protocol::extentid_t id, int &);
+    int put(extent_protocol::extentid_t id, std::string, int &);
+
+    int get(extent_protocol::extentid_t id, std::string &);
+
+    int getattr(extent_protocol::extentid_t id, extent_protocol::attr &);
+
+    int remove(extent_protocol::extentid_t id, int &);
 };
 
-#endif 
-
-
-
-
-
-
-
+#endif
