8b71bcf885ab0ac8d3c6b1db8bf7bf2ff4a4ad0e
Daniel.Emmel <s8daemme@stud.uni-saarland.de>
2021-07-01 02:51:18 +0200

    implemented lab6 step1
    Co-authored-by: Moritz Wilhelm <s8mowilh@stud.uni-saarland.de>

--- extent_client.cc
+++ extent_client.cc
@@ -3,6 +3,7 @@
 #include "extent_client.h"
 #include <sstream>
 #include <iostream>
+#include <utility>
 #include <stdio.h>
 #include <unistd.h>
 #include <time.h>
@@ -16,37 +17,107 @@ extent_client::extent_client(std::string dst) {
     if (cl->bind() != 0) {
         printf("extent_client: bind failed\n");
     }
+
+    assert(pthread_mutex_init(&this->storage_lock, nullptr) == 0);
+}
+
+extent_client::~extent_client() {
+    assert(pthread_mutex_destroy(&this->storage_lock) == 0);
+}
+
+extent_protocol::status extent_client::put(extent_protocol::extentid_t eid, std::string buf) {
+    ScopedLock scopedLock(&this->storage_lock);
+
+    Extent extent;
+    if (this->storage.find(eid) != this->storage.end() && this->storage.at(eid).present) {
+        extent = this->storage.at(eid);
+
+        // update metadata
+        time_t current_time = std::time(nullptr);
+        extent.metadata.mtime = current_time;
+        extent.metadata.ctime = current_time;
+    } else {
+        extent.is_new = true;
+    }
+    extent.content = buf;
+    extent.metadata.size = buf.size();
+    extent.dirty = true;
+    this->storage[eid] = extent;
+    return extent_protocol::OK;
 }
 
 extent_protocol::status extent_client::get(extent_protocol::extentid_t eid, std::string &buf) {
+    ScopedLock sl(&this->storage_lock);
     extent_protocol::status ret = extent_protocol::OK;
-    ret = cl->call(extent_protocol::get, eid, buf);
+
+    if (this->storage.find(eid) != this->storage.end() && this->storage.at(eid).present) {
+        // update access time
+        Extent &extent = this->storage.at(eid);
+        extent.metadata.atime = std::time(nullptr);
+        extent.dirty = true;
+
+        buf = extent.content;
+    } else {
+        Extent extent;
+        ret = cl->call(extent_protocol::get, eid, extent.content);
+        if (ret != extent_protocol::OK) {
+            return extent_protocol::NOENT;
+        }
+        ret = cl->call(extent_protocol::getattr, eid, extent.metadata);
+        if (ret != extent_protocol::OK) {
+            return extent_protocol::NOENT;
+        }
+        this->storage.emplace(eid, extent);
+    }
     return ret;
 }
 
 extent_protocol::status extent_client::getattr(extent_protocol::extentid_t eid, extent_protocol::attr &attr) {
+    ScopedLock scopedLock(&this->storage_lock);
     extent_protocol::status ret = extent_protocol::OK;
-    ret = cl->call(extent_protocol::getattr, eid, attr);
+
+    if (this->storage.find(eid) == this->storage.end() || !this->storage.at(eid).present) {
+        Extent extent;
+        ret = cl->call(extent_protocol::getattr, eid, extent.metadata);
+        if (ret != extent_protocol::OK) {
+            return extent_protocol::NOENT;
+        }
+        ret = cl->call(extent_protocol::get, eid, extent.content);
+        if (ret != extent_protocol::OK) {
+            return extent_protocol::NOENT;
+        }
+        this->storage.emplace(eid, extent);
+    }
+
+    const extent_protocol::attr &data = this->storage.at(eid).metadata;
+    attr = data;
     return ret;
 }
 
 extent_protocol::status extent_client::setattr(extent_protocol::extentid_t eid, extent_protocol::attr attr) {
-    int r;
-    return cl->call(extent_protocol::setattr, eid, attr, r);
-}
+    ScopedLock scopedLock(&this->storage_lock);
 
-extent_protocol::status extent_client::put(extent_protocol::extentid_t eid, std::string buf) {
-    extent_protocol::status ret = extent_protocol::OK;
-    int r;
-    ret = cl->call(extent_protocol::put, eid, buf, r);
-    return ret;
+    if (this->storage.find(eid) != this->storage.end() && this->storage.at(eid).present) {
+        Extent &extent = this->storage.at(eid);
+        extent.content.resize(attr.size);
+        extent.metadata = attr;
+        extent.dirty = true;
+        return extent_protocol::OK;
+    }
+    return extent_protocol::NOENT;
 }
 
 extent_protocol::status extent_client::remove(extent_protocol::extentid_t eid) {
-    extent_protocol::status ret = extent_protocol::OK;
-    int r;
-    ret = cl->call(extent_protocol::remove, eid, r);
-    return ret;
+    ScopedLock scopedLock(&this->storage_lock);
+
+    //TODO: on synchronization, only sync remove if extent was not new
+    //TODO: on synchronization, remove from storage if not present
+
+    if (this->storage.find(eid) != this->storage.end() && this->storage.at(eid).present) {
+        Extent &extent = this->storage.at(eid);
+        extent.present = false;
+    }
+    return extent_protocol::OK;
 }
 
 extent_protocol::status extent_client::get_next_id(extent_protocol::extentid_t id, unsigned long &res) {
--- extent_client.h
+++ extent_client.h
@@ -6,21 +6,45 @@
 #include <string>
 #include "extent_protocol.h"
 #include "rpc.h"
+#include <unordered_map>
+#include <utility>
+#include <ctime>
+#include "pthread.h"
 
 class extent_client {
 private:
     rpcc *cl;
 
+    struct Extent {
+        // indicates whether Extent was created locally or not
+        bool is_new = false;
+
+        bool present = true;
+        bool dirty = false;
+        std::string content{};
+        extent_protocol::attr metadata{};
+
+        Extent() {
+            unsigned int time = std::time(nullptr);
+            this->metadata = {time, time, time, 0};
+        }
+    };
+
+    pthread_mutex_t storage_lock;
+    std::unordered_map<extent_protocol::extentid_t, Extent> storage;
+
 public:
     extent_client(std::string dst);
+
+    ~extent_client();
+
+    extent_protocol::status put(extent_protocol::extentid_t eid, std::string buf);
 
     extent_protocol::status get(extent_protocol::extentid_t eid, std::string &buf);
 
     extent_protocol::status getattr(extent_protocol::extentid_t eid, extent_protocol::attr &a);
 
     extent_protocol::status setattr(extent_protocol::extentid_t eid, extent_protocol::attr a);
-
-    extent_protocol::status put(extent_protocol::extentid_t eid, std::string buf);
 
     extent_protocol::status remove(extent_protocol::extentid_t eid);
 
